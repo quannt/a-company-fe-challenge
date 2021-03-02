b3c66c1bf37df577494e0500333e349e26cd11d9
quannt <simpleplan.vn@gmail.com>
2021-03-03 00:06:37 +0900

    Remove vue-select

--- packages/app-vue/nuxt.config.js
+++ packages/app-vue/nuxt.config.js
@@ -15,8 +15,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: "~/plugins/v-tooltip.js", mode: "client" },
-    { src: "~/plugins/vue-select.js", mode: "client" }
+    { src: "~/plugins/v-tooltip.js", mode: "client" }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
